5d8e7c051cc889d3b5a9ee3c1d91f03f9eaf81ed
yushuai1 <yushuai1@xiaomi.com>
2023-09-17 19:08:54 +0800

    test

--- src/pages/Messag3.jsx
+++ src/pages/Messag3.jsx
@@ -1,7 +1,9 @@
+import { Button } from 'antd'
 import React, { useState } from 'react'
-import { Link, Outlet } from 'react-router-dom'
+import { Link, Outlet,useNavigate } from 'react-router-dom'
 
 export default function Message3() {
+  const navigate = useNavigate()
   const [messages] = useState(
     [
       { id: '001', title: 'msg01', content: 'xxxxx01' },
@@ -10,6 +12,25 @@ export default function Message3() {
       { id: '004', title: 'msg04', content: 'xxxxx04' }
     ]
   )
+  function showdetail(m){
+   navigate('detail3',{
+    replace:false,
+    state:{
+      id:m.id,
+      title:m.title,
+      content:m.content
+    }
+   })
+  }
+
+  function back(){
+    navigate(-1)
+
+  }
+  function forward(){
+    navigate(1)
+    
+  }
 
   return (
     <div>
@@ -25,14 +46,20 @@ export default function Message3() {
                     title: m.title,
                     content: m.content,
                   }}>
-                  {m.title}
+                  {m.title} 
                 </Link>
+                <span> </span>
+                <Button onClick={()=>showdetail(m)}>Get Detail</Button>
               </li>
             )
           }
           )
         }
+       
       </ul>
+      <Button onClick={back}>后退</Button>
+      <span> </span>
+      <Button onClick={forward}>前进</Button>
       <hr />
       <Outlet />
 
