e8d740d05ada86edce1c106083cc8b320a6af3a9
yushuai1_m <yushuai1@xiaomi.com>
2023-09-25 09:17:23 +0800

    update console

--- src/pages/routerBase/About.jsx
+++ src/pages/routerBase/About.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
+import { useInRouterContext } from 'react-router-dom'
 
 export default function About() {
+  console.log("是否在路由组件中： ",useInRouterContext())
   return (
     <div>
       I am about
--- src/pages/routerBase/News.jsx
+++ src/pages/routerBase/News.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 import { useNavigationType, useResolvedPath } from 'react-router-dom'
 
 export default function News() {
-    console.log("useNavigationType: ", useNavigationType())
-    console.log("resolve path: ", useResolvedPath('/user?id=001&name=test'))
+    console.log("通过哪种路由方式访问此页面，useNavigationType: ", useNavigationType())
+    console.log("resolve path: ", useResolvedPath('/user?id=001&name=test#qwe'))
     return (
         <div>
             <ul>
