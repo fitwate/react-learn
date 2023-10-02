9ab07832d3141cf8a3e20ea9d4b49e8c9d032f6b
yushuai1 <yushuai1@xiaomi.com>
2023-10-02 14:57:20 +0800

    test

--- src/pages/reactDocs/Sharingdata.jsx
+++ src/pages/reactDocs/Sharingdata.jsx
@@ -1,25 +1,25 @@
-import React,{ useState } from 'react'
+import React, { useState } from 'react'
+
+function MyButton({ count, onClick }) {
+  return (
+    <button onClick={onClick}>
+      Clicked {count} times
+    </button>
+  );
+}
 
 export default function Sharingdata() {
-    const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0);
 
-    function handleClick() {
-      setCount(count + 1);
-    }
-  
-    return (
-      <div>
-        <h1>Counters that update together</h1>
-        <MyButton count={count} onClick={handleClick} />
-        <MyButton count={count} onClick={handleClick} />
-      </div>
-    );
+  function handleClick() {
+    setCount(count + 1);
   }
-  
-  function MyButton({ count, onClick }) {
-    return (
-      <button onClick={onClick}>
-        Clicked {count} times
-      </button>
-    );
+
+  return (
+    <div>
+      <h1>Counters that update together</h1>
+      <MyButton count={count} onClick={handleClick} />
+      <MyButton count={count} onClick={handleClick} />
+    </div>
+  );
 }
