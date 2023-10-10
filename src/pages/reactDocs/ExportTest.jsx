6190f8aa7f592b00b821cc449ebf41d72a56f525
yushuai1_m <yushuai1@xiaomi.com>
2023-10-10 09:22:52 +0800

    test

--- src/pages/reactDocs/ExportTest.jsx
+++ src/pages/reactDocs/ExportTest.jsx
@@ -2,11 +2,23 @@ import React from 'react'
 import Gallery from './Gallery';
 import { Profile } from './Gallery';
 
+// 嵌套组件
+function Card({ children }) {
+  return (
+    <div >
+     <h1>嵌套组件</h1>
+      {children}
+    </div>
+  );
+}
+
 export default function ExportTest() {
   return (
-    <div>
+    <Card>
       <Profile />
-      <Gallery />
-    </div>
+      <Gallery
+        propsV1="test props transfer"
+      />
+    </Card>
   )
 }
